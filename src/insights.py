59b73b7c72d89d6a4c38aa64325dacfccc66430b
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-20 18:07:31 -0300

    feat: filter by job type
    return the jobs relate to the type setted

--- src/insights.py
+++ src/insights.py
@@ -19,21 +19,7 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return [job for job in jobs if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):

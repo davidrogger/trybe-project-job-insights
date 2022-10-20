4585a831b0e704c70f862d88daac391d5d370ffa
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-20 15:29:10 -0300

    feat: created method read
    return a list from a csv file

--- src/jobs.py
+++ src/jobs.py
@@ -1,7 +1,11 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    'Initial commit'
-    return []
+    with open(f'../{path}', "r") as file:
+        dict_jobs = csv.DictReader(file)
+        list_jobs = [*dict_jobs]
+
+    return list_jobs

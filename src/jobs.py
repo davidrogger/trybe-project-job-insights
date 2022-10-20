cab549c022e71a2156ed8df9766daa14d7173119
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-20 14:36:21 -0300

    initial commit

--- src/jobs.py
+++ src/jobs.py
@@ -3,16 +3,5 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
+    'Initial commit'
     return []

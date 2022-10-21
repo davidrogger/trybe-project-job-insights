ec082c6e28f8f1ccaa0571264566b59b8b35b853
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-21 08:52:04 -0300

    test: counter words

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,23 @@
-# from src.counter import count_ocurrences
+from unittest.mock import patch, mock_open
+import pytest
+from src.counter import count_ocurrences
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def context_controlled():
+    return [
+        "Python python python Python",
+        "Missing Typescript :(",
+        "Always good to practice! Practice is life.",
+    ]
+
+
+def test_counter(context_controlled):
+    with patch("builtins.open", mock_open(read_data=context_controlled[0])):
+        assert count_ocurrences("src/jobs.csv", "PYTHON") == 4
+
+    with patch("builtins.open", mock_open(read_data=context_controlled[1])):
+        assert count_ocurrences("src/jobs.csv", "typescript") == 1
+
+    with patch("builtins.open", mock_open(read_data=context_controlled[2])):
+        assert count_ocurrences("src/jobs.csv", "Practice") == 2

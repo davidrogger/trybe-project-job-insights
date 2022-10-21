190587538b3be716a17e39202664cb9babc808fd
Davíd Roggér <davidrogger.a@gmail.com>
2022-10-21 09:31:02 -0300

    test: brazilian column translate
    testing method that translate columns to english

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,18 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs_dict_english = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    columns = [
+        {"en": "title", "pt": "titulo"},
+        {"en": "salary", "pt": "salario"},
+        {"en": "type", "pt": "tipo"},
+    ]
+
+    for job in jobs_dict_english:
+        for column in columns:
+            should_be_true = column["en"] in job
+            should_be_false = column["pt"] in job
+
+            assert should_be_true is True
+            assert should_be_false is False
